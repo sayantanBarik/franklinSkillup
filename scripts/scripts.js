330c9e2d61128faf523f3a4b5393eecdfab9cb13
Sayantan Barik <sayantan.barik@netcentric.biz>
2024-03-01 17:23:49 +0530

    Two columns

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -46,10 +46,14 @@ async function loadFonts() {
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
-function buildAutoBlocks(main) {
+async function buildAutoBlocks(main) {
   try {
     buildHeroBlock(main);
-    
+    const template = await import('../templates/two-columns/two-columns.js');
+    if (template.default) {
+      await template.default(main);
+    }
+
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
